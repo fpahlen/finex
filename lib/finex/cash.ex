7708682930a4f7ce293cb8fd7e9326f90574ade8
fpahlen <fpahlen@fredriks-pro.lan>
2020-07-25 21:10:18 +0200

    remove git test code

--- lib/finex/cash.ex
+++ lib/finex/cash.ex
@@ -5,7 +5,4 @@ defmodule Finex.Cash do
   def from_string(<<currency_code::binary-size(3), " ", amt::binary>>) do
     %Finex.Cash{currency: currency_code, amount: String.to_float(amt)}
   end
-  def to_string() do
-    "Häpp"
-  end
 end
